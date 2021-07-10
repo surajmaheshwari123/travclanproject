ae0db36f66268c2e9219f21ec184d36cf23407d1
SurajMaheshwari <Surajmaheshwari1101@gmail.com>
2021-07-10 12:06:52 +0530

    Customer all bids

--- src/components/eachmerchant.jsx
+++ src/components/eachmerchant.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
- 
- 
+ import React from 'react';
 function  EachMerchant(props) {
- console.log(props)
-
+ console.log(props.location.state)
   return (  
-    <React.Fragment>  
-    <h1>Not Found</h1>
- 
-  </React.Fragment>
+<div>
+    <h2>Customer Bid</h2>
+{props.location.state.merchant.bids.map(x=>(
+
+<div class="card" style={{ marginBottom:20}}>
+  <div class="container">
+    <h4><b>Amount:{x.amount}</b></h4>
+    <p>Title:{x.carTitle}</p>
+    <p>Created:{x.created}</p>
+    <p>Id:{x.id}</p>    
+  </div>
+</div>
+)
+
+)}
+</div>
+
+  
   );
 }
 
